c3208baa81a9d35932cce8717b3c230bdb21ec42
Mewantha <mewa.bandara@gmail.com>
2017-02-04 17:55:38 +0530

    Added CSS

--- runThisOnLoad.js
+++ runThisOnLoad.js
@@ -1,8 +1,6 @@
 //Run this when web page is loaded to check if the site is allowed or blocked
-
-		
-	    var urlOfTheCurrentWindow = window.location.href;
-	    var nameOfTheSite = urlOfTheCurrentWindow.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
-	    if (localStorage.getItem("isBlocked") == 1){
-			alert('You are not allowed to come to '+nameOfTheSite+'. Prepare to be zapped.');
-		}
+var urlOfTheCurrentWindow = window.location.href;
+var nameOfTheSite = urlOfTheCurrentWindow.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
+if (localStorage.getItem("isBlocked") == 1){
+	alert('You are not allowed to come to '+nameOfTheSite+'. Prepare to be zapped.');
+}
