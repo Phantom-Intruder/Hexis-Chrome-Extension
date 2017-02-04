28a66b452642e3976c37196efa562b179a0c7568
Mewantha <mewa.bandara@gmail.com>
2017-02-04 16:52:47 +0530

    Got the blocking working

--- popup.js
+++ popup.js
@@ -1,14 +1,9 @@
+
 function onSettingsButtonClick(){
 	window.open('settings.html', '_blank');
 }
 
-function onAllowPageButtonClick(){
-chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var urlOfTheCurrentWindow = tabs[0].url;	   
 
-	   alert("Allowed the page "+urlOfTheCurrentWindow+ " only");
-});
-}
 
 function onAllowSiteButtonClick(){
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
@@ -16,27 +11,31 @@ chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var domain = urlOfTheCurrentWindow.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
 
 	   alert("Allowed the whole site "+domain);
+       allowDomain();
 });
 }
 
-function onBlockPageButtonClick(){
-chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var urlOfTheCurrentWindow = tabs[0].url;	   
-
-	   alert("Blocked the page "+urlOfTheCurrentWindow+ " only");
-	   chrome.storage.sync.set({ urlOfTheCurrentWindow:urlOfTheCurrentWindow });
-
-});
+function allowDomain(){
+    chrome.tabs.executeScript(null, {file: "removeFromStorage.js"});
 }
 
 function onBlockSiteButtonClick(){
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var urlOfTheCurrentWindow = tabs[0].url;	   
     var domain = urlOfTheCurrentWindow.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
+	   blockDomain();
+});
+}
 
-	   alert("Blocked the whole site "+domain);
-	   chrome.storage.sync.set({ urlOfTheCurrentWindow:urlOfTheCurrentWindow });
+function blockDomain(){
+	chrome.tabs.executeScript(null, {file: "addToStorage.js"});
+}
 
+function checkButton(){
+	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var urlOfTheCurrentWindow = tabs[0].url;	   
+    var domain = urlOfTheCurrentWindow.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
+    //check localstorage	
 });
 }
 
@@ -47,17 +46,14 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('allowButtonPage').addEventListener('click', onAllowPageButtonClick);
-});
-
-document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('allowButtonSite').addEventListener('click', onAllowSiteButtonClick);
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('blockButtonPage').addEventListener('click', onBlockPageButtonClick);
-});
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('blockButtonSite').addEventListener('click', onBlockSiteButtonClick);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('checkButton').addEventListener('click', checkButton);
 });
--- runThisOnLoad.js
+++ runThisOnLoad.js
@@ -1,9 +1,8 @@
 //Run this when web page is loaded to check if the site is allowed or blocked
 
-	
+		
 	    var urlOfTheCurrentWindow = window.location.href;
 	    var domain = urlOfTheCurrentWindow.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
-	    if (domain === 'www.youtube.com'){
+	    if (localStorage.getItem("isBlocked") == 1){
 			alert('You are not allowed to come to '+domain+'. Prepare to be zapped.');
 		}
-
