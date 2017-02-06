a82cab5a0235c5e10c64f3f7929f56d052c0e6e3
Mewantha <mewa.bandara@gmail.com>
2017-02-06 20:13:34 +0530

    Added comment look at runThisOnLoad.js

--- runThisOnLoad.js
+++ runThisOnLoad.js
@@ -3,4 +3,5 @@ var urlOfTheCurrentWindow = window.location.href;
 var nameOfTheSite = urlOfTheCurrentWindow.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
 if (localStorage.getItem("isBlocked") == 1){
 	alert('You are not allowed to come to '+nameOfTheSite+'. Prepare to be zapped.');
+	//The code should run here
 }
